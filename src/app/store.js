61024d9bd183dffaf3bff86660557243af99c28f
Rahulmsutar <rahulsutar959@gmail.com>
2022-07-22 20:50:53 +0530

    complete

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/CounterSlice'
 import coinReducer from '../features/coin/CoinSlice'
 import themeReducer from '../features/theme/ThemeSlice'
+
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
